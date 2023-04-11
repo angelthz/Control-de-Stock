e644a715445f1251eeabb048bde27228be99df18
angelthz <angeltorreshernan@gmail.com>
2023-04-11 16:49:31 -0600

    Se cambio la clase ConnectionFactory, en lugar de devolver una conexion a la DB mediante el JDBC ahora se devuelve a traves de un POOL de conexiones.

--- src/main/java/com/alura/jdbc/connection/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/connection/ConnectionFactory.java
@@ -4,18 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	private DataSource dataSource;
+	
 	//POOL de conexiones
-	
-	
+	public ConnectionFactory() {
+		ComboPooledDataSource pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("2011.1994");
+		
+		this.dataSource = pooledDataSource;
+	}
 	
 	public Connection getConection() throws SQLException {
-			return DriverManager.getConnection(
-			        "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-			        "root",
-			        "2011.1994");
+		return this.dataSource.getConnection();
 	}
 }
